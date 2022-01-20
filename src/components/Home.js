97087ad2123d0422f0ce1b9f616f7f1ce5dfdb9d
Noman Ali <noman710@protonmail.com>
2022-01-20 16:34:44 -0500

    Navbar

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react'
+
+export default function Home() {
+    return (
+        <Fragment>
+            <h1 className="text-center mt-2">Welcome</h1>
+        </Fragment>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,43 @@
+import React, { Fragment } from "react";
+import {Link} from 'react-router-dom'
+
+export default function Nav() {
+  return (
+    <Fragment>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <img src="https://pngimg.com/uploads/nba/nba_PNG15.png" width="1%" className="img me-2"></img>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <p className="mt-3 navbar-brand">NBA Gatcha</p>
+              </li>
+            </ul>
+            <ul className="navbar-nav mb-2 mb-lg-0">
+                <Link to="/">
+                <li className="nav-item me-4 home">
+                    <a className="navbar-brand" href="#">Home</a>
+                </li>
+                </Link>
+                <Link to="/store">
+                <li className="nav-item me-4 store">
+                    <a className="navbar-brand" href="#">Store</a>
+                </li>
+                </Link>
+                <Link to="/collection">
+                <li className="nav-item me-4 collection">
+                    <a className="navbar-brand" href="#">My Collection</a>
+                </li>
+                </Link>
+                <Link to="login">
+                <li className="nav-item me-4 login">
+                    <a className="navbar-brand" href="#">Login</a>
+                </li>
+                </Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
+  );
+}
