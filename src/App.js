9a63608340c3195a0799ef83f4368c8412b178a0
Noman Ali <noman710@protonmail.com>
2022-01-24 16:00:31 -0500

    Pack 3 retro done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Store from "./components/Store";
 import Signup from "./components/Signup";
 import Pack1 from "./components/Pack1";
 import Pack2 from "./components/Pack2";
+import Pack3 from "./components/Pack3";
 //Animation
 import "aos/dist/aos.css"
 function App() {
@@ -23,6 +24,7 @@ function App() {
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/pack1" element={<Pack1/>}/>
         <Route path="/pack2" element={<Pack2/>}/>
+        <Route path="/pack3" element={<Pack3/>}/>
       </Routes>
     </BrowserRouter>
 
